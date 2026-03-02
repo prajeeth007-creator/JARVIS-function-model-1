627312e7bb892f1cddfe9126be1dbfd28ced58bd
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-03-02 11:48:03 +0530

    modified file

--- PHANTOM/index.py
+++ PHANTOM/index.py
@@ -1,16 +1,17 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# ===== PHANTOM AI =====
 bot = ChatBot(
-    "HARSH",
+    "PHANTOM",
     read_only=False,
     logic_adapters=[
         {
             "import_path": "chatterbot.logic.BestMatch",
-            "default_response": "I am sorry, I don't have an answer for that.",
+            "default_response": "I'm not sure how to respond to that yet.",
             "maximum_similarity_threshold": 0.90
         }
     ]
@@ -19,18 +20,16 @@ bot = ChatBot(
 trainer = ChatterBotCorpusTrainer(bot)
 trainer.train("chatterbot.corpus.english")
 
+# ===== ROUTES =====
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/get")
-def get_chatbot_response():
-    userText = request.args.get('userMessage')
-    return str(bot.get_response(userText))
-
-
-
+def get_response():
+    user_text = request.args.get("userMessage")
+    response = bot.get_response(user_text)
+    return str(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
- 
